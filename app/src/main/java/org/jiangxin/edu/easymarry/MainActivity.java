70072bd2662d132c126756b382310759ad4772c3
jiangxincode <jiangxinnju@163.com>
2018-01-07 22:50:59 +0800

    add action bar

--- app/src/main/java/org/jiangxin/edu/easymarry/MainActivity.java
+++ app/src/main/java/org/jiangxin/edu/easymarry/MainActivity.java
@@ -1,9 +1,12 @@
 package org.jiangxin.edu.easymarry;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ListView;
@@ -22,12 +25,6 @@ public class MainActivity extends AppCompatActivity {
     private ListView mHomeListView;
 
     private ViewGroup viewGroup;
-
-    private String[] name = {"找车", "找饭店", "找宾馆", "买车票"};
-
-    private String[] desc = {"找车", "找饭店", "找宾馆", "买车票"};
-
-    private int[] images = {R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
 
     private SimpleAdapter simpleAdapter = null;
 
@@ -86,32 +83,28 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getActionBar();
+
         mHomeListView = (ListView) findViewById(R.id.home_list_view);
         mTextMessage = (TextView) findViewById(R.id.message);
 
 
         viewGroup = (ViewGroup) mTextMessage.getParent();
 
-        List<Map<String, Object>> items = new ArrayList<Map<String, Object>>();
+        String[] name = {"找车", "找饭店", "找宾馆", "买车票"};
+        String[] desc = {"找车", "找饭店", "找宾馆", "买车票"};
+        int[] images = {R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
+
+        List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
         for (int i = 0; i < name.length; i++) {
             Map<String, Object> item = new HashMap<String, Object>();
             item.put("head", images[i]);
             item.put("name", name[i]);
             item.put("desc", desc[i]);
-            items.add(item);
+            data.add(item);
         }
 
-        /*SimpleAdapter的参数说明
-         * 第一个参数 表示访问整个android应用程序接口，基本上所有的组件都需要
-         * 第二个参数表示生成一个Map(String ,Object)列表选项
-         * 第三个参数表示界面布局的id  表示该文件作为列表项的组件
-         * 第四个参数表示该Map对象的哪些key对应value来生成列表项
-         * 第五个参数表示来填充的组件 Map对象key对应的资源一依次填充组件 顺序有对应关系
-         * 注意的是map对象可以key可以找不到 但组件的必须要有资源填充  因为 找不到key也会返回null 其实就相当于给了一个null资源
-         * 下面的程序中如果 new String[] { "name", "head", "desc","name" } new int[] {R.id.name,R.id.head,R.id.desc,R.id.head}
-         * 这个head的组件会被name资源覆盖
-         * */
-        simpleAdapter = new SimpleAdapter(this, items,
+        simpleAdapter = new SimpleAdapter(this, data,
                 R.layout.home_listview, new String[]{"name", "head", "desc"},
                 new int[]{R.id.name, R.id.head, R.id.desc});
 
@@ -120,6 +113,13 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
 
