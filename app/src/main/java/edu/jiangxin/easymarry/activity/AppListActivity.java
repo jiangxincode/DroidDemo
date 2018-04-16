2453a9eb5558e8d336f10bbe04f79cd18dcde85c
jiangxincode <jiangxinnju@163.com>
2018-04-17 00:44:04 +0800

    获取系统应用列表

--- app/src/main/java/edu/jiangxin/easymarry/activity/AppListActivity.java
+++ app/src/main/java/edu/jiangxin/easymarry/activity/AppListActivity.java
@@ -1,13 +1,12 @@
 package edu.jiangxin.easymarry.activity;
 
-import java.io.ByteArrayInputStream;
-import java.security.MessageDigest;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
-
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextPaint;
 import android.view.KeyEvent;
@@ -22,12 +21,13 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
+
+import java.io.ByteArrayInputStream;
+import java.security.MessageDigest;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.security.auth.x500.X500Principal;
 
@@ -172,16 +172,10 @@ public class AppListActivity extends Activity implements OnClickListener {
 		// 管理应用程序包
 		PackageManager pManager = mContext.getPackageManager();
         // 获取手机内所有应用
-        List<PackageInfo> packlist = pManager.getInstalledPackages(0);
-        for (int i = 0; i < packlist.size(); i++) {
-            PackageInfo pak = packlist.get(i);
-            // 判断是否为非系统预装的应用程序
-            // 这里还可以添加系统自带的，这里就先不添加了，如果有需要可以自己添加
-            // if()里的值如果<=0则为自己装的程序，否则为系统工程自带
-            if ((pak.applicationInfo.flags & pak.applicationInfo.FLAG_SYSTEM) <= 0) {
-                // 添加自己已经安装的应用程序
-                this.listPInfos.add(pak);
-            }
+        List<PackageInfo> packageInfos = pManager.getInstalledPackages(0);
+        for (PackageInfo packageInfo : packageInfos) {
+			// packageInfo.applicationInfo.flags & packageInfo.applicationInfo.FLAG_SYSTEM  如果<=0则为自己装的程序，否则为系统工程自带
+			this.listPInfos.add(packageInfo);
         }
 	}
 	
