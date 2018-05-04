031e21bf54c001b989cbfce1830a072352d77575
jiangxincode <jiangxinnju@163.com>
2018-05-04 20:26:50 +0800

    修改Drawable对象颜色

--- app/src/main/java/edu/jiangxin/easymarry/activity/DialogActivity.java
+++ app/src/main/java/edu/jiangxin/easymarry/activity/DialogActivity.java
@@ -1,7 +1,10 @@
 package edu.jiangxin.easymarry.activity;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import edu.jiangxin.easymarry.R;
 
@@ -11,5 +14,10 @@ public class DialogActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
+
+        TextView tv = findViewById(R.id.tv_tint);
+        Drawable bg = tv.getBackground();
+        // 修改Drawable对象颜色
+        bg.setTint(Color.BLUE);
     }
 }
