25adee50dc43893e608b2d637a74189933fbc76a
Aloys <jiangxinnju@163.com>
2021-02-14 20:19:11 +0800

    add Makefile print local variable demo

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,8 +1,11 @@
- LOCAL_PATH := $(call my-dir) 	#提定当前路径
+LOCAL_PATH := $(call my-dir) 	#提定当前路径
 
- include $(CLEAR_VARS)			#清除全局配置变量，LOCAL_XXX,除了LOCAL_PATH
+include $(CLEAR_VARS)			#清除全局配置变量，LOCAL_XXX,除了LOCAL_PATH
 
- LOCAL_MODULE    := hello		#指定生成动态库名hello，生成的动态库文件libhello.so
- LOCAL_SRC_FILES := hello.c		#指定生成动态库的源文件
+LOCAL_MODULE    := hello		#指定生成动态库名hello，生成的动态库文件libhello.so
+LOCAL_SRC_FILES := hello.c		#指定生成动态库的源文件
 
- include $(BUILD_SHARED_LIBRARY) #提定生成动态库
+include $(BUILD_SHARED_LIBRARY) #提定生成动态库
+
+# print variable in makefile
+$(warning " LOCAL_PATH is $(LOCAL_PATH)")
