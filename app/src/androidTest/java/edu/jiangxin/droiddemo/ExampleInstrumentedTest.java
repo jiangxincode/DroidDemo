85a6bc92094595a92486e1053f23ae602d8306d1
Aloys <jiangxinnju@163.com>
2023-04-22 11:14:34 +0800

    update ci conf

--- app/src/androidTest/java/edu/jiangxin/droiddemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/jiangxin/droiddemo/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("org.jiangxin.edu.droiddemo", appContext.getPackageName());
+        assertEquals("edu.jiangxin.droiddemo", appContext.getPackageName());
     }
 }
