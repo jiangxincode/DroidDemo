e8d0e3c092941676c9274ac7f1d44b8758fee2cf
Aloys <jiangxinnju@163.com>
2024-03-30 21:45:37 +0800

    optimize SensorActivity.java

--- app/src/main/java/edu/jiangxin/droiddemo/SensorActivity.java
+++ app/src/main/java/edu/jiangxin/droiddemo/SensorActivity.java
@@ -7,53 +7,76 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SensorActivity extends Activity {
     private static final String TAG = "SensorActivity";
 
-    private SensorManager sm = null;
+    private SensorManager mSensorManager = null;
 
-    private List<SensorEventListener> listeners = new ArrayList<>();
+    private final Map<String, SensorEventListener> mSensorEventListenerMap = new HashMap<>();
 
-    private LinearLayout linearLayout;
+    private LinearLayout mLinearLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         setContentView(R.layout.activity_sensor);
-        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        mLinearLayout = findViewById(R.id.linearLayout);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        List<Sensor> sensorList = sm.getSensorList(Sensor.TYPE_ALL);
+        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         for (Sensor sensor : sensorList) {
-            TextView textView = new TextView(this);
-            textView.setText(sensor.getName());
-            SensorEventListener listener = new MySensorEventListener(textView);
-            listeners.add(listener);
-            sm.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-            linearLayout.addView(textView);
+            LinearLayout itemLayout = new LinearLayout(this);
+            itemLayout.setOrientation(LinearLayout.VERTICAL);
+
+            TextView sensorNameTextView = new TextView(this);
+            sensorNameTextView.setText(sensor.getName());
+
+            TextView sensorDataTextView = new TextView(this);
+
+            mSensorEventListenerMap.put(sensor.getName(), new MySensorEventListener(sensorDataTextView));
+
+            Button button = new Button(this);
+            button.setText("register");
+            button.setOnClickListener(v -> {
+                if (button.getText().equals("register")) {
+                    button.setText("unregister");
+                    mSensorManager.registerListener(mSensorEventListenerMap.get(sensor.getName()), sensor, SensorManager.SENSOR_DELAY_NORMAL);
+                } else {
+                    button.setText("register");
+                    mSensorManager.unregisterListener(mSensorEventListenerMap.get(sensor.getName()));
+                }
+            });
+
+            itemLayout.addView(sensorNameTextView);
+            itemLayout.addView(button);
+            itemLayout.addView(sensorDataTextView);
+
+            mLinearLayout.addView(itemLayout);
         }
     }
 
     @Override
     protected void onStop() {
-        for (SensorEventListener listener : listeners) {
-            sm.unregisterListener(listener);
+        for (SensorEventListener listener : mSensorEventListenerMap.values()) {
+            mSensorManager.unregisterListener(listener);
         }
         super.onStop();
     }
 
     static class MySensorEventListener implements SensorEventListener {
-        private TextView mTextView;
+        private final TextView mTextView;
         MySensorEventListener(TextView textView) {
             super();
             mTextView = textView;
@@ -63,9 +86,8 @@ public class SensorActivity extends Activity {
         public void onSensorChanged(SensorEvent event) {
             synchronized (this) {
                 float[] values = event.values;
-                Sensor sensor = event.sensor;
                 StringBuilder sb = new StringBuilder();
-                sb.append(sensor.getName()).append("[");
+                sb.append("[");
                 for (int i = 0; i < values.length; i++) {
                     sb.append(values[i]);
                     if (i < values.length - 1) {
